43117d10818b2c74269d9c3bafffff148375161e
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-31 04:40:40 +0200

    ham : Build QTI HIDL power 1.0 HAL
     * Despite ham  have QCACLD2 WLAN card and the node that should
       return WiFi stats is available, it seems that the firmware
       doesn't fully supports it, causing freezes when trying to
       access the node. Just go with the HAL that provides no
       support for this feature.
    
    Change-Id: I929a2d3aedf3bfdca962dc34720019d83c7134e1

--- ham.mk
+++ ham.mk
@@ -202,7 +202,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti 
+     android.hardware.power@1.0-service-qti
 
 # Sensors
 PRODUCT_PACKAGES += \
